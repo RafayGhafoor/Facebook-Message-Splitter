ab14ab1f9ea5b0ccfecbeb381edc541366d48f94
Rafay Ghafoor <rafayghafoor@protonmail.com>
2017-06-15 14:50:38 +0500

    Create fms.py

--- fms.py
+++ fms.py
@@ -1,6 +1,10 @@
 import bs4
 import os
-import requests
+import mechanize
+import sys
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 para_text = []
 chat_participants = []
@@ -38,21 +42,38 @@ def openfile(filename="test.htm"):
 
 
 def get_profile_names(user_id):
+    url = "https://facebook.com/" + user_id
     '''Get profile name from userid. For Example:-
     >>> 100004561272818 [INPUT]
     >>> Alan Walker [OUTPUT]
     '''
-    pass
+    br = mechanize.Browser()
+    br.set_handle_equiv(True)
+    br.set_handle_gzip(True)
+    br.set_handle_redirect(True)
+    br.set_handle_referer(True)
+    br.set_handle_robots(False)
+    br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0')]
+    br.open('https://www.facebook.com/')
+    br.select_form(nr=0)
+    br['email'] = 'email'
+    br['pass'] = 'passwd'
+    res = br.submit()
+    print "Successfully Logged in!\n"
+    soup = bs4.BeautifulSoup(br.open(url).read(), 'html.parser')
+    return soup.find("title").text.encode('ascii', 'ignore')
 
 
-    
-def uid_pnames(): # UserID to Profile Names
+def uid_pnames():	    # UserID to Profile Names
     '''Replace UserIDs with Profile Names'''
     pass
 
 
 def main():
-    openfile()
-    format_messages()
+    # openfile()
+    # format_messages()
+    name = get_profile_names("100004528276712")
+    print "%r" % name
 
 main()
